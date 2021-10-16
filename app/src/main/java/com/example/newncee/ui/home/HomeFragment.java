affa38e389301b2961dbc81315be6d7c9413bfc4
Sorades <2019302080119@whu.edu.cn>
2021-10-16 22:53:08 +0800

    隐藏actionbar

--- app/src/main/java/com/example/newncee/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/newncee/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.newncee.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +9,8 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -32,4 +35,5 @@ public class HomeFragment extends Fragment {
         });
         return root;
     }
+
 }
